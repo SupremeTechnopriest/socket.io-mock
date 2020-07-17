738483edd5a49c2a3738105216381e2549d57418
Randy Lebeau <rl@Technos-MacBook-Pro-2.local>
2020-07-17 10:01:48 -0700

    ✨Update dependencies.

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,5 +1,15 @@
-function createCommonjsModule(fn, module) {
-	return module = { exports: {} }, fn(module, module.exports), module.exports;
+function createCommonjsModule(fn, basedir, module) {
+	return module = {
+	  path: basedir,
+	  exports: {},
+	  require: function (path, base) {
+      return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+    }
+	}, fn(module, module.exports), module.exports;
+}
+
+function commonjsRequire () {
+	throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
 }
 
 var componentEmitter = createCommonjsModule(function (module) {

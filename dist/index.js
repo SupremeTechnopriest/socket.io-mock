74c8de82335919a77354a924a1a8f9a8257e8d8b
Matteo Gheza <matteo.gheza07@gmail.com>
2021-07-13 10:54:55 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -461,7 +461,7 @@ Object.defineProperty(Emittery, 'listenerRemoved', {
 	configurable: false
 });
 
-class SocketClient extends emittery {
+class SocketClient extends Emittery {
   /**
    * A mocking class for the Socket IO Client side
    * @param {SocketMock} socketMock
@@ -469,7 +469,7 @@ class SocketClient extends emittery {
   constructor (socketMock) {
     super();
     this._socketMock = socketMock;
-    this._emitFn = emittery.prototype.emit;
+    this._emitFn = Emittery.prototype.emit;
     this.connected = true;
     this.disconnected = false;
   }
@@ -523,7 +523,7 @@ const createPayload = function (object) {
 /**
  * A mocking class for the Socket IO Server side
  */
-class SocketMock extends emittery {
+class SocketMock extends Emittery {
   /**
    * Creates a new SocketMock instance
   **/
@@ -531,7 +531,7 @@ class SocketMock extends emittery {
     super();
     this.joinedRooms = this.rooms = [];
     this.socketClient = new SocketClient(this);
-    this._emitFn = emittery.prototype.emit;
+    this._emitFn = Emittery.prototype.emit;
     this.generalCallbacks = {};
     this.broadcast = {
       /**
